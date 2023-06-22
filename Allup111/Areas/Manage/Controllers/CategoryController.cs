ca5e339aa04a0aa0ef827a5da1a16bc6f9bdc88f
MuradHacizada <myemail>
2023-06-22 18:56:23 +0400

    Category Create Action Maded

--- Allup111/Areas/Manage/Controllers/CategoryController.cs
+++ Allup111/Areas/Manage/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Allup111.DAL;
 using Allup111.Models;
+using Allup111.Utilities.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,9 +10,12 @@ namespace Allup111.Areas.Manage.Controllers
     public class CategoryController : Controller
     {
         private readonly AppDbContext _db;
-        public CategoryController(AppDbContext db)
+        private readonly IWebHostEnvironment _env;
+        public CategoryController(AppDbContext db, IWebHostEnvironment env)
         {
+
             _db = db;
+            _env = env;
         }
         public async Task<IActionResult> Index()
         {
@@ -23,6 +27,8 @@ namespace Allup111.Areas.Manage.Controllers
             ViewBag.MainCategories = await _db.Categories.Where(x => x.IsMain).ToListAsync();
             return View();
         }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
         public async Task<IActionResult> Create(Category category ,int? mainCatId)
         {
             ViewBag.MainCategories = await _db.Categories.Where(x => x.IsMain).ToListAsync();
@@ -34,11 +40,32 @@ namespace Allup111.Areas.Manage.Controllers
                     ModelState.AddModelError("Name", "This Category is already exist");
                     return View();
                 }
+                #region Save Image
+                if (category.Photo == null)
+                {
+                    ModelState.AddModelError("Photo", "Image can not be null");
+                    return View();
+                }
+                if (!category.Photo.IsImage())
+                {
+                    ModelState.AddModelError("Photo", "Pls select image type");
+                    return View();
+                }
+                if (category.Photo.IsOlderMb())
+                {
+                    ModelState.AddModelError("Photo", "max 1 mb");
+                    return View();
+                }
+                string folder = Path.Combine(_env.WebRootPath,"assets", "images");
+                category.Image = await category.Photo.SaveFileAsync(folder);
+                #endregion
             }
             else
             {
-
+                category.ParentId = mainCatId;
             }
+            await _db.Categories.AddAsync(category);
+            await _db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
     }
---
+++ Allup111/Utilities/Extensions/FileExtension.cs
@@ -0,0 +1,25 @@
+﻿namespace Allup111.Utilities.Extensions
+{
+    public static class FileExtension
+    {
+        public static bool IsImage(this IFormFile file)
+        {
+            return file.ContentType.Contains("image/");
+
+        }
+        public static bool IsOlderMb(this IFormFile file)
+        {
+            return file.Length / 1024 > 1024;
+        }
+        public static async Task<string> SaveFileAsync(this IFormFile file, string folder)
+        {
+            string filename = Guid.NewGuid().ToString() + file.FileName;
+            string path = Path.Combine(folder, filename);
+            using (FileStream fileStream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+            return filename;
+        }
+    }
+}
